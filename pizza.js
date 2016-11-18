8895a9d50b20d9767d1b0ab55702812c1e83ce44
elim2513 <8elim@stpatrick.org>
2016-11-18 10:50:33 -0600

    Add files via upload
    Update

--- pizza.js
+++ pizza.js
@@ -1,3 +1,25 @@
 class pizza{
-  constructor(s,mt,vt);
+  constructor(s,mt,lt){
+    this.size=s;
+    this.meatToppings=mt;
+    this.veggieToppings=vt;
+  }
+  sizecost(){
+    if(this.size == "Small"){
+      this.sizeCost = 7.99;
+      return sizeCost;
+    }
+    else if(this.size == "Medium"){
+      this.sizeCost = 9.99;
+      return sizeCost;
+    }
+    else if(this.size == "Large"){
+      this.sizeCost = 12.99;
+      return sizeCost;
+    }
+    else if(this.size == "Extra-Large"){
+      this.sizeCost= 15.99;
+      return sizeCost;
+    }
+  }
 }
