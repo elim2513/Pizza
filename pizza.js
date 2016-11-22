a7d734b294063febad95694a15ef051ba0af6788
elim2513 <8elim@stpatrick.org>
2016-11-22 10:25:37 -0600

    Add files via upload
    Update 3.0 final

--- pizza.js
+++ pizza.js
@@ -6,7 +6,7 @@ class pizza{
   }
   sizecost(){
     if(this.size == "Small"){
-    return 7.99;
+      return 7.99;
     }
     else if(this.size == "Medium"){
       return 7.99;
@@ -30,7 +30,15 @@ toppingCost(top){
     }
   }
 price(){
-  let totalcost = cost+toppingCost;
-  return totalcost;
+  return this.costsize() + this.toppingCost("meat") + this.toppingCost("veggie")
+}
+static promotionDeal(pizza,percent){
+  let decimal = percent/100;
+  return pizza.price() -pizza.price()*decimal;
 }
 }
+
+let mypizza = new Pizza() ("Medium",["sausage","pepperoni"],["mushroom","blackolives","greenpeppers"]);
+
+console.log(mypizza instanceof Pizza);
+console.log(mypizza.price());
